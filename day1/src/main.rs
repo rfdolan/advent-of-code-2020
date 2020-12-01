a243115c7142a010a89de28dd6224a9ebbeba9f3
Raymond <rfdolan@wpi.edu>
2020-12-01 12:17:03 -0500

    Better day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -4,27 +4,33 @@ use std::path::Path;
 use std::vec::Vec;
 
 fn main() {
-
-    let mut vec = Vec::new();
-    if let Ok(lines) = read_lines("day1.txt") {
-        for line in lines {
-            if let Ok(ip) = line {
-                let line_num: u32 = ip.parse().expect("Not a number!");
-                vec.push(line_num);
-            }
-        }
-    }
+    let vec = parse_file("day1.txt");
     for int1 in vec.iter() {
         for int2 in vec.iter() {
-            for int3 in vec.iter() {
-                if (int1 + int2 + int3) == 2020 {
-                    println!("{}", int1*int2*int3);
-                }
+            let target = 2020 - int1 -int2;
+            if vec.contains(&target) {
+                println!("{}", int1*int2*target);
+                return;
             }
         }
     }
 }
 
+// Parse file with given name in parent directory into a vector of ints
+fn parse_file(name: &str) -> Vec<i32> {
+    let mut vec = Vec::new();
+    if let Ok(lines) = read_lines(name) {
+        for line in lines {
+            if let Ok(ip) = line {
+                let line_num: i32 = ip.parse().expect("Not a number!");
+                vec.push(line_num);
+            }
+        }
+    }
+    vec
+}
+
+// Read each line of a file
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
     let file = File::open(filename)?;
