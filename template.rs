e56a69abb938535990f2b347c606b6d75e4b76ec
Raymond <rfdolan@wpi.edu>
2020-12-27 02:17:51 -0500

    day22

--- template.rs
+++ template.rs
@@ -3,16 +3,16 @@ use std::vec::Vec;
 fn main(){
     let vec = inp::parse_file("xxx.txt");
     // Put the code to do the thing here
-    part1();
-    part2();
+    part1(&vec);
+    part2(&vec);
 }
 
 // Solution for part 1
-fn part1() {
+fn part1(input: &Vec<String>) {
     println!("Part 1: {}", 0);
 }
 
 // Solution for part 2
-fn part2() {
+fn part2(input: &Vec<String>) {
     println!("Part 2: {}", 0);
 }
